a9e6503c7793e80d57023f1af8d916491a3d45c1
Wassim <wassimmansour@hotmail.com>
2020-04-26 18:47:23 -0500

    multiple numbers with mixed delimiters

--- StringCalculator/BL.Tests/CalculatorTests.cs
+++ StringCalculator/BL.Tests/CalculatorTests.cs
@@ -5,11 +5,64 @@ namespace BL.Tests
 	public class CalculatorTests
 	{
 		[Test]
-		public void SimpleAdd()
+		[Order(1)]
+		public void EmptyString()
 		{
-			string numbers = "1,3";
+			string numbers = "";
 
-			int expected = 4;
+			int expected = 0;
+
+			int actual = Calculator.Add(numbers);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		[Order(2)]
+		public void SingleNumber()
+		{
+			string numbers = "24";
+
+			int expected = 24;
+
+			int actual = Calculator.Add(numbers);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		[Order(3)]
+		public void MulipleNumbersWithComma()
+		{
+			string numbers = "11,22,303";
+
+			int expected = 336;
+
+			int actual = Calculator.Add(numbers);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		[Order(4)]
+		public void MulipleNumbersWithNewLine()
+		{
+			string numbers = "11\n22\n303";
+
+			int expected = 336;
+
+			int actual = Calculator.Add(numbers);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		[Order(5)]
+		public void MulipleNumbersWithMixedDelimiters()
+		{
+			string numbers = "11\n22,303";
+
+			int expected = 336;
 
 			int actual = Calculator.Add(numbers);
 
--- StringCalculator/BL/Calculator.cs
+++ StringCalculator/BL/Calculator.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace BL
 {
 	public class Calculator
 	{
-		public static int Add(string numbers)
+		public static int Add(string numbersString)
 		{
-			return 0;
+			if (string.IsNullOrWhiteSpace(numbersString))
+			{
+				return 0;
+			}
+
+			List<int> numbersArray = GetNumbers(numbersString, new List<string>());
+
+			return numbersArray.Sum();
+
+		}
+
+		private static List<int> GetNumbers(string numbersString, List<string> delimiters)
+		{
+			// Default delimiters: ',' and '\n'
+			if(delimiters.Count() == 0)
+			{
+				return numbersString.Trim()
+						.Split(',')
+						.SelectMany(x => x.Split("\n"))
+						.Select(x => {
+							int.TryParse(x, out int result);
+							return result;
+						})
+						.ToList();
+			}
+			// Custom delimiter
+			else
+			{
+				return null;
+			}
 		}
 	}
 }
