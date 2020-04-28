257fcf785ff9694b952213d2fa65b31e40755295
Wassim <wassimmansour@hotmail.com>
2020-04-27 21:00:44 -0500

    Added muliples

---
+++ FizzBuzz/BL.Tests/SimpleGameTests.cs
@@ -0,0 +1,111 @@
+using NUnit.Framework;
+
+namespace BL.Tests
+{
+	public class SimpleGameTests
+	{
+		[Test]
+		public void ReturnsSameNumberTest()
+		{
+			var number = 1;
+
+			var expected = "1";
+
+			SimpleGame game = new SimpleGame();
+			var actual = game.Play(number);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void MultiplesOfThree()
+		{
+			var number = 27;
+
+			var expected = "Fizz";
+
+			SimpleGame game = new SimpleGame();
+			var actual = game.Play(number);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void MultiplesOfFive()
+		{
+			var number = 25;
+
+			var expected = "Buzz";
+
+			SimpleGame game = new SimpleGame();
+			var actual = game.Play(number);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void MultiplesOfThreeAndFive()
+		{
+			var number = 15;
+
+			var expected = "Fizz Buzz";
+
+			SimpleGame game = new SimpleGame();
+			var actual = game.Play(number);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void MultiplesOfSeven()
+		{
+			var number = 14;
+
+			var expected = "Pop";
+
+			SimpleGame game = new SimpleGame();
+			var actual = game.Play(number);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void MultiplesOfThreeAndSeven()
+		{
+			var number = 21;
+
+			var expected = "Fizz Pop";
+
+			SimpleGame game = new SimpleGame();
+			var actual = game.Play(number);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void MultiplesOfFiveAndSeven()
+		{
+			var number = 35;
+
+			var expected = "Buzz Pop";
+
+			SimpleGame game = new SimpleGame();
+			var actual = game.Play(number);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void MultiplesOfThreeAndFiveAndSeven()
+		{
+			var number = 210;
+
+			var expected = "Fizz Buzz Pop";
+
+			SimpleGame game = new SimpleGame();
+			var actual = game.Play(number);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ FizzBuzz/BL/SimpleGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL
+{
+	public class SimpleGame
+	{
+		public string Play(int number)
+		{
+			List<string> result = new List<string>();
+			if (number % 3 == 0)
+			{
+				result.Add("Fizz");
+			}
+			if (number % 5 == 0)
+			{
+				result.Add("Buzz");
+			}
+			if (number % 7 == 0)
+			{
+				result.Add("Pop");
+			}
+
+			if (result.Count() > 0)
+			{
+				return result.Aggregate((acc, next) => $"{acc} {next}");
+			}
+			else
+			{
+				return number.ToString();
+			}
+		}
+	}
+}
